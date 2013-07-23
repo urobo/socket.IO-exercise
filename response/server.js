7359432a12849418e8831709ce78cec275bf887f
urobo <rico.sleeps@gmail.com>
2013-07-23 06:49:37 +0200

    chat 0.0.1

--- response/server.js
+++ response/server.js
@@ -5,5 +5,16 @@ var app = connect().use(connect.static('public')).listen(3000);
 var chat_room = io.listen(app);
 
 chat_room.sockets.on('connection', function(socket){
-	chat_room.sockets.emit('entrance', {message: 'A new chatter in online.'});
+	socket.emit('entrance', {message: 'Welcome to the chat room'});
+
+	socket.on('disconnect', function(){
+		chat_room.sockets.emit('exit', {message: 'A chatter has left.'});
+	});
+
+	socket.on('chat', function(data){
+		chat_room.sockets.emit('chat', {message: '#' + data.message});
+	});
+
+	chat_room.sockets.emit('entrance', {message: 'A new chatter is online'});
 });
+
